fd8d36d52ef289d3d49fba45e9642b375aaaa8c8
rizkyz <rizkyz@gmail.com>
2024-08-26 02:26:44 +0000

    feat : add more flags

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -10,5 +10,252 @@ final List<Flag> flags = [
     src:
         'https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg',
   ),
-  // Add more flags here...
+  Flag(
+    country: 'Kanada',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/c/cf/Flag_of_Canada.svg',
+  ),
+  Flag(
+    country: 'Inggris',
+    src:
+        'https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg',
+  ),
+  Flag(
+    country: 'Prancis',
+    src: 'https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg',
+  ),
+  Flag(
+    country: 'Jerman',
+    src: 'https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg',
+  ),
+  Flag(
+    country: 'Italia',
+    src: 'https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg',
+  ),
+  Flag(
+    country: 'Jepang',
+    src: 'https://upload.wikimedia.org/wikipedia/en/9/9e/Flag_of_Japan.svg',
+  ),
+  Flag(
+    country: 'Australia',
+    src: 'https://upload.wikimedia.org/wikipedia/en/b/b9/Flag_of_Australia.svg',
+  ),
+  Flag(
+    country: 'Brazil',
+    src: 'https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg',
+  ),
+  Flag(
+    country: 'India',
+    src: 'https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg',
+  ),
+  Flag(
+    country: 'Tiongkok',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+  ),
+  Flag(
+    country: 'Korea Selatan',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg',
+  ),
+  Flag(
+    country: 'Afrika Selatan',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/a/af/Flag_of_South_Africa.svg',
+  ),
+  Flag(
+    country: 'Indonesia',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg',
+  ),
+  Flag(
+    country: 'Meksiko',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg',
+  ),
+  Flag(
+    country: 'Argentina',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg',
+  ),
+  Flag(
+    country: 'Spanyol',
+    src: 'https://upload.wikimedia.org/wikipedia/en/9/9a/Flag_of_Spain.svg',
+  ),
+  Flag(
+    country: 'Nigeria',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/7/79/Flag_of_Nigeria.svg',
+  ),
+  Flag(
+    country: 'Arab Saudi',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/0/0d/Flag_of_Saudi_Arabia.svg',
+  ),
+  Flag(
+    country: 'Mesir',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_Egypt.svg',
+  ),
+  Flag(
+    country: 'Turki',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/b/b4/Flag_of_Turkey.svg',
+  ),
+  Flag(
+    country: 'Thailand',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_Thailand.svg',
+  ),
+  Flag(
+    country: 'Vietnam',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Vietnam.svg',
+  ),
+  Flag(
+    country: 'Filipina',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_the_Philippines.svg',
+  ),
+  Flag(
+    country: 'Malaysia',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/6/66/Flag_of_Malaysia.svg',
+  ),
+  Flag(
+    country: 'Singapura',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg',
+  ),
+  Flag(
+    country: 'Pakistan',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/3/32/Flag_of_Pakistan.svg',
+  ),
+  Flag(
+    country: 'Bangladesh',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/f/f9/Flag_of_Bangladesh.svg',
+  ),
+  Flag(
+    country: 'Iran',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/c/ca/Flag_of_Iran.svg',
+  ),
+  Flag(
+    country: 'Irak',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Flag_of_Iraq.svg',
+  ),
+  Flag(
+    country: 'Portugal',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg',
+  ),
+  Flag(
+    country: 'Belanda',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg',
+  ),
+  Flag(
+    country: 'Belgia',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Belgium.svg',
+  ),
+  Flag(
+    country: 'Swiss',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Switzerland.svg',
+  ),
+  Flag(
+    country: 'Swedia',
+    src: 'https://upload.wikimedia.org/wikipedia/en/4/4c/Flag_of_Sweden.svg',
+  ),
+  Flag(
+    country: 'Norwegia',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Norway.svg',
+  ),
+  Flag(
+    country: 'Denmark',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/9/9c/Flag_of_Denmark.svg',
+  ),
+  Flag(
+    country: 'Finlandia',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/b/bc/Flag_of_Finland.svg',
+  ),
+  Flag(
+    country: 'Austria',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_Austria.svg',
+  ),
+  Flag(
+    country: 'Yunani',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Greece.svg',
+  ),
+  Flag(
+    country: 'Polandia',
+    src: 'https://upload.wikimedia.org/wikipedia/en/1/12/Flag_of_Poland.svg',
+  ),
+  Flag(
+    country: 'Hungaria',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/c/c1/Flag_of_Hungary.svg',
+  ),
+  Flag(
+    country: 'Republik Ceko',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_the_Czech_Republic.svg',
+  ),
+  Flag(
+    country: 'Slowakia',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/e/e6/Flag_of_Slovakia.svg',
+  ),
+  Flag(
+    country: 'Ukraina',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/4/49/Flag_of_Ukraine.svg',
+  ),
+  Flag(
+    country: 'Romania',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/7/73/Flag_of_Romania.svg',
+  ),
+  Flag(
+    country: 'Bulgaria',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Bulgaria.svg',
+  ),
+  Flag(
+    country: 'Kroasia',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/1/1b/Flag_of_Croatia.svg',
+  ),
+  Flag(
+    country: 'Serbia',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/f/ff/Flag_of_Serbia.svg',
+  ),
+  Flag(
+    country: 'Montenegro',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/6/64/Flag_of_Montenegro.svg',
+  ),
+  Flag(
+    country: 'Slovenia',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/f/f0/Flag_of_Slovenia.svg',
+  ),
+  Flag(
+    country: 'Bosnia dan Herzegovina',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/b/bf/Flag_of_Bosnia_and_Herzegovina.svg',
+  ),
+  Flag(
+    country: 'Makedonia Utara',
+    src:
+        'https://upload.wikimedia.org/wikipedia/commons/1/1f/Flag_of_North_Macedonia.svg',
+  ),
 ];
